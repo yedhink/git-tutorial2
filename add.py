8f8aaaca6c85fa20cc6da945af8ff9c8fc45328f
YEDHIN KIZHAKKETHARA <18616@fisat>
2018-04-18 01:10:57 -0400

    add function for 3

--- add.py
+++ add.py
@@ -1,6 +1,16 @@
 def add(num1, num2):
     return num1+num2
+def add3(num1, num2,num3):
+    return num1+num2+num3
 
-a = int(raw_input("Enter 1st = "))
-b = int(raw_input("Enter 2nd = "))
-print "sum = " add(a,b)
+print "add\n1) 2 numbers\n2) 3 numbers\n"
+choice = raw_input("Enter choice = ")
+if choice == '1':
+    a = int(raw_input("Enter 1st = "))
+    b = int(raw_input("Enter 2nd = "))
+    print "sum = ",add(a,b)
+else:
+    a = int(raw_input("Enter 1st = "))
+    b = int(raw_input("Enter 2nd = "))
+    c = int(raw_input("Enter 3rd = "))
+    print "sum = ",add(a,b,c)
